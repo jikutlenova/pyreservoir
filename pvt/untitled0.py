4ebbd969849e3d2289abb60c6e837e5a014ef3a7
jiku tlenova <priyankape2005@gmail.com>
2020-09-20 12:22:23 +0530

    Added pseudopressure module to your PVT library.........(have not validated it....as inital correltaion only work for press. >154 psia)

---
+++ pvt/untitled0.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 11:20:57 2020
+
+@author: Priyank.Srivastava
+"""
+
+import os
+os.chdir("/Users/priyank.srivastava/Desktop/PEG-Python/PVT")
+import numpy as np
+from pvt_gas import *
+
+pressure = 2010 # psi
+temp = 110 # Fahrenheit
+sg = 0.7 # specific gravity
+x_h2s = 0.07 # mole fraction of H2S in gas
+x_co2 = 0.1 # mole fraction of CO2 in gas
+
+
+# calculate gas z-factor 
+pseudo_rho, z_factor = gas_zfactor(temp, pressure, sg, x_h2s, x_co2)
+# calculate gas density
+rhogas = gas_density(temp, pressure, sg, x_h2s, x_co2)
+# calculate gas viscosity 
+viscogas = gas_mu(temp, pressure, sg, x_h2s, x_co2)
+mp(temp, pressure, sg, x_h2s, x_co2)
